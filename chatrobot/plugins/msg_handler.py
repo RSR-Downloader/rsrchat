3674311777bed97ee5913fa334fa5e074d539971
RSR-Downloader <79363075+RSR-Downloader@users.noreply.github.com>
2021-03-06 20:19:57 +0530

    Update msg_handler.py

--- chatrobot/plugins/msg_handler.py
+++ chatrobot/plugins/msg_handler.py
@@ -39,7 +39,7 @@ async def all_messages_catcher(event):
                 )
             )
         except telethon.errors.rpcerrorlist.UserNotParticipantError:
-            await event.reply(f"**To Message My Master, Please Join My Channel. :)**",
+            await event.reply(f"**Ka Master hnen ah message thawn ani, Channel join rawh. :)**",
                              buttons = [Button.url("Join Channel", Config.JMTC_LINK)])
             return
     await event.get_sender()
